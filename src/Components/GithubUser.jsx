f5fb1541ecf2152241d0d6db5adab9504d5d2389
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-10-05 23:12:07 +0100

    UI Completed

--- src/Components/GithubUser.jsx
+++ src/Components/GithubUser.jsx
@@ -6,44 +6,60 @@ const GithubUser = () => {
   return (
     <div className='User'>
         <div>
-          <img src="https://avatars.githubusercontent.com/u/107879861?v=4" alt="avatar" />
+          <img 
+          src="https://avatars.githubusercontent.com/u/107879861?v=4" 
+          alt="avatar" />
         </div>
 
         <div>
           <div className='UserName'>
-              <p style={{fontSize: "20px", fontWeight: "700"}}>The Octocat</p>
+              <p 
+               style={{
+                fontSize: "20px", 
+                fontWeight: "700"}}>The Octocat</p>
               <p>Joined 25 Jan 2011</p>
           </div>
-              <a href="">@Octocat</a>
+              <a 
+              href="" 
+              style={{
+                color: "blue"}}>@Octocat</a>
               <p>This profile has no bio</p>
 
           <div className='UserDetails'>
               <div>
                 <p>Repos</p>
-                <p style={{fontSize: "20px", fontWeight: "700"}}>8</p>
+                <p style={{
+                  fontSize: "20px", 
+                  fontWeight: "700"}}>8</p>
               </div>
 
               <div>
                 <p>Followers</p>
-                <p style={{fontSize: "20px", fontWeight: "700"}}>3938</p>
+                <p style={{
+                  fontSize: "20px", 
+                  fontWeight: "700"}}>3938</p>
               </div>
 
               <div>
                 <p>Following</p>
-                <p style={{fontSize: "20px", fontWeight: "700"}}>9</p>
+                <p style={{
+                  fontSize: "20px", 
+                  fontWeight: "700"}}>9</p>
               </div>
           </div>
 
           <div className='UserSocials'>
             <div className='Social1'>
-            <a href=""><FaMapMarkerAlt/> San Francisco</a>
+            <span><FaMapMarkerAlt/> San Francisco</span>
                <a href=""><FaTwitter/> Not Available</a>
           
             </div>
 
               <div className='Social2'>
-              <a href=""><FaLink/> https://github.blog</a>
-             <a href=""><FaRegBuilding/> @github</a>
+              <a href="">
+                <FaLink/> https://github.blog</a>
+             <span>
+              <FaRegBuilding/> @github</span>
             </div>
           </div>
         </div>
