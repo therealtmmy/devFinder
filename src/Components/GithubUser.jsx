4620ec05ff1b645d5b37c41c6f163a112c3a1d2d
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-10-27 02:00:23 +0100

    adding func

--- src/Components/GithubUser.jsx
+++ src/Components/GithubUser.jsx
@@ -1,65 +1,66 @@
 import React from 'react'
 import { FaMapMarkerAlt, FaTwitter, FaLink, FaRegBuilding } from "react-icons/fa";
 import "../Components/GithubUser.css"
+import user from "./user.json"
 
 const GithubUser = () => {
+  const Styling = {
+    fontSize: "20px", 
+    fontWeight: "700"
+  }
+
   return (
     <div className='User'>
         <div>
           <img 
-          src="https://avatars.githubusercontent.com/u/107879861?v=4" 
+          src={user.avatar_url}
           alt="avatar" />
         </div>
 
         <div>
           <div className='UserName'>
               <p 
-               style={{
-                fontSize: "20px", 
-                fontWeight: "700"}}>The Octocat</p>
-              <p>Joined 25 Jan 2011</p>
+               style={Styling}>{user.name}</p>
+              <p>Joined 21 Jan 2001</p>
           </div>
               <a 
-              href="" 
-              style={{
-                color: "blue"}}>@Octocat</a>
-              <p>This profile has no bio</p>
+              href={user.html_url} 
+              style={{color: 'blue'}}>@{user.login}</a>
+
+      <p>
+        {user.bio ? (<p>{user.bio}</p> ) 
+        : (<p>This profile has no bio</p> )}
+      </p>
 
           <div className='UserDetails'>
               <div>
                 <p>Repos</p>
-                <p style={{
-                  fontSize: "20px", 
-                  fontWeight: "700"}}>8</p>
+                <p style={Styling}>{user.public_repos}</p>
               </div>
 
               <div>
                 <p>Followers</p>
-                <p style={{
-                  fontSize: "20px", 
-                  fontWeight: "700"}}>3938</p>
+                <p style={Styling}>{user.followers}</p>
               </div>
 
               <div>
                 <p>Following</p>
-                <p style={{
-                  fontSize: "20px", 
-                  fontWeight: "700"}}>9</p>
+                <p style={Styling}>{user.following}</p>
               </div>
           </div>
 
           <div className='UserSocials'>
             <div className='Social1'>
-            <span><FaMapMarkerAlt/> San Francisco</span>
-               <a href=""><FaTwitter/> Not Available</a>
+            <span><FaMapMarkerAlt/> {user.location}</span>
+               <a href=""><FaTwitter/>{user.twitter_username ? (<p>{user.twitter_username}</p>) : (<span> Unavailable</span>)}</a>
           
             </div>
 
               <div className='Social2'>
-              <a href="">
+              <a href="https://github.blog">
                 <FaLink/> https://github.blog</a>
              <span>
-              <FaRegBuilding/> @github</span>
+              <FaRegBuilding/> {user.company ? (<span>{user.company}</span>) : (<span> Unavailable </span>)}</span>
             </div>
           </div>
         </div>
--- src/Components/SearchUser.jsx
+++ src/Components/SearchUser.jsx
@@ -1,23 +1,43 @@
 import React from 'react'
-import {FaRegLightbulb} from "react-icons/fa";
+import {FaMoon, FaRegLightbulb} from "react-icons/fa";
 import {FaSearch} from "react-icons/fa";
 import "../Components/SearchUser.css"
+import { useState } from 'react';
 
 const SearchUser = () => {
+  const [show, setShow] = useState(true)
+  const [search, setSearch] = useState("")
+  const toggleShow = () => {
+    setShow(!show)
+  }
+
+  const onSearch = () => {
+    alert(`${search}`)
+  }
+
   return (
     <div className='SearchUser'>
       <div className='Header'>
           <p>devfinder</p>
-          <button className='Switch'>LIGHT <FaRegLightbulb /></button>
+          {show ? (<button className='Switch' onClick={toggleShow}>LIGHT <FaRegLightbulb /></button>) :  (<button className='Switch' onClick={toggleShow}>DARK <FaMoon /></button>)}
       </div>
 
+   <form onSubmit={onSearch}>
       <div className='Search'>
         <div className='inputField'>
         <FaSearch style={{color: "#0077FF", fontSize: "20px"}}/>
-        <input type="text"placeholder='Search GitHub Username...'/>
+
+      <input 
+        type="text"
+        placeholder ='Search GitHub Username...'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        />
+
         </div>
         <button className='SearchBtn'>Search</button>
       </div>
+      </form> 
     </div>
   )
 }
